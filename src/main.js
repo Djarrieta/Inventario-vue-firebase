cb6edc5d4ca723ed0caa19459551909d4fc93405
Dario Arrieta <arrieta.dario@hotmail.com>
2020-08-07 17:23:50 -0500

    tailwind configurado con el plugin @ianaya89/tailwind

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue' //importa npm
+import App from './App.vue' //importa app.vue
+import "@/assets/css/tailwind.css" //@ hace referencia a src de manera relativa
 
 Vue.config.productionTip = false
 
+//corresponde a la configuracion el, data 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
